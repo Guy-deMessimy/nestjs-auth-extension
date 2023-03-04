6b69dc5c41d1886239de8eb5cc06cd49aff9d875
Guy de Messimy <gdemessimy@gmail.com>
2023-03-04 18:33:20 +0100

    feature : app module

---
+++ src/app.module.ts
@@ -0,0 +1,31 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { CoffeesModule } from './coffees/coffees.module';
+import { UsersModule } from './users/users.module';
+import { IamModule } from './iam/iam.module';
+import { ConfigModule } from '@nestjs/config';
+
+// best practice : throw an exception during application startup if any required env have not been provided or don(t meet certain validation rules
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    CoffeesModule,
+    UsersModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'pass123',
+      database: 'postgres',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    IamModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
