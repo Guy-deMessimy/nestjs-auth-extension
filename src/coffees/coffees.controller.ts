4fe3caf96eb42b4408bbe348689de54212ede6e8
Guy de Messimy <gdemessimy@gmail.com>
2023-03-04 18:32:33 +0100

    feature : coffee entuty

---
+++ src/coffees/coffees.controller.ts
@@ -0,0 +1,60 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Req,
+} from '@nestjs/common';
+import { Auth } from 'src/iam/authentication/decorators/auth.decorator';
+import { AuthType } from 'src/iam/authentication/enums/auth-type.enum';
+import { ActiveUserData } from 'src/iam/authentication/interfaces/active-user-data.interface';
+import { Permissions } from 'src/iam/authorization/decorators/permission.decorator';
+import { Policies } from 'src/iam/authorization/decorators/policies.decorator';
+import { Roles } from 'src/iam/authorization/decorators/roles.decorators';
+import { Permission } from 'src/iam/authorization/permission.type';
+import { FrameworkContributorPolicy } from 'src/iam/authorization/policies/framework-contributor.policy';
+import { ActiveUser } from 'src/iam/decorators/active-user.decorator';
+import { Role } from 'src/users/enums/role.enum';
+import { CoffeesService } from './coffees.service';
+import { CreateCoffeeDto } from './dto/create-coffee.dto';
+import { UpdateCoffeeDto } from './dto/update-coffee.dto';
+
+@Controller('coffees')
+export class CoffeesController {
+  constructor(private readonly coffeesService: CoffeesService) {}
+  // @Roles(Role.Admin)
+  // @Permissions(Permission.CreateCoffee)
+  @Policies(
+    new FrameworkContributorPolicy() /** new MinAgePolicy(18), new OnlyAdminPolicy() */,
+  )
+  @Post()
+  create(@Body() createCoffeeDto: CreateCoffeeDto) {
+    return this.coffeesService.create(createCoffeeDto);
+  }
+
+  @Get()
+  findAll(@ActiveUser() user: ActiveUserData) {
+    console.log(user);
+    return this.coffeesService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.coffeesService.findOne(+id);
+  }
+
+  @Roles(Role.Admin)
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateCoffeeDto: UpdateCoffeeDto) {
+    return this.coffeesService.update(+id, updateCoffeeDto);
+  }
+
+  @Roles(Role.Admin)
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.coffeesService.remove(+id);
+  }
+}
