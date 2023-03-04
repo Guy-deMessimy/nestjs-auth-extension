f27ac4d3a7c224502fba064cde590ccaffbe9ab6
Guy de Messimy <gdemessimy@gmail.com>
2023-03-04 18:32:56 +0100

    feature : coffee entuty

---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,27 @@
+import {
+  Permission,
+  PermissionType,
+} from 'src/iam/authorization/permission.type';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Role } from '../enums/role.enum';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column({ enum: Role, default: Role.Regular })
+  role: Role;
+
+  // NOTE: Having the "permissions" column in combination with the "role"
+  // likely does not make sense. We use both in this course just to showcase
+  // two different approaches to authorization.
+  @Column({ enum: Permission, default: [], type: 'json' })
+  permissions: PermissionType[];
+}
